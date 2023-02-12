90f2157069a6c47ea94e4110d229d259ca2bab9c
Dissanayake71 <85305564+Dissanayake71@users.noreply.github.com>
2023-02-12 21:57:57 +0530

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1> 
+      <p>IMDb is an online database of information 
+        related 
+        to films, television series, music videos,
+         podcasts, home videos, video games, and 
+         streaming content online – including cast,
+          production crew and personal biographies, 
+          plot summaries, trivia, ratings, and fan 
+          and critical reviews.</p> 
+    <p>
+Launched online in 1990 and a subsidiary
+ of Amazon.com since 1998, IMDb is the
+  world's most popular and authoritative
+   source for movie, TV and celebrity
+   content, designed to help fans explore 
+   the world 
+of movies and shows and decide what to watch.</p>
+     
+    </div>
+  )
+}
