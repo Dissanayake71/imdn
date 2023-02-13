e8909633b35416ec778042b66d8d433424658472
Dissanayake71 <85305564+Dissanayake71@users.noreply.github.com>
2023-02-13 22:22:12 +0530

    type error corrction

--- src/app/Card.jsx
+++ src/app/Card.jsx
@@ -19,7 +19,7 @@ export default function Card({result}) {
         ></Image>
         <div className="p-2 ">
             <p className="line-clamp-2 text-md">{result.overview}</p>
-            <h2 className="truncate text-lg font-bold">{result.title || title.name}</h2>
+            <h2 className="truncate text-lg font-bold">{result.title || result.name}</h2>
             <p className="flex items-center">{result.release_date || result.first_air_date}
             <FiThumbsUp className=" h-5 mr-1 ml-3"/>{result.vote_count}</p>
         </div>
